c6a4fede2f2c7dea4b6867eab65b6ad31fd0fb2d
aissaelouafi <aissaelouafi1994@gmail.com>
2017-07-10 00:09:12 +0200

    Try to apply the NaiveBayesClassifier to a Dstream

--- PySparkStreaming/prediction/stream_prediction.py
+++ PySparkStreaming/prediction/stream_prediction.py
@@ -14,16 +14,33 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField
 from pyspark.sql.types import DoubleType, IntegerType, StringType
 
+from pyspark.sql import Row, SQLContext
+
+
+
+def predict(rdd):
+    count = rdd.count()
+    if(count>0):
+        hashingTF = HashingTF(numFeatures=1000)
+        features = hashingTF.transform(rdd.map(lambda doc:doc["text"].split(" "))).
+        result = model.transform(features)
+        print result.probability
+        #Here write result to SOLR
+    else:
+        print("No data receveid")
+
+
 #Create a local spark context and a streaming context
 sc = SparkContext("local[3]","PySparkStreamingTest")
 ssc = StreamingContext(sc,15)
+spark = SparkSession.builder.appName("Real Time Tweets Sentiment Analysis").getOrCreate()
 
 #Set Log Level to ERROR
 sc.setLogLevel("ERROR")
 
+
 #Import the Naive bayes classifiers model
 model = NaiveBayesModel.load(sc, "ML_models/NaiveClassifier/naiveBayesClassifier-2010-09-10-08-51-25")
-
 
 #Initialize the kafka stream
 lines = ssc.socketTextStream("localhost", 9999)
@@ -34,54 +51,55 @@ lines = ssc.socketTextStream("localhost", 9999)
 
 #Define the schema of the local context dataframe
 schema = StructType([
-    StructField("SentimentText", IntegerType())
+    StructField("SentimentText", StringType())
 ])
 
 
-try:
-    #Parse string to json object
-    tweets = lines.map(lambda v: json.loads(v))
+#Parse string to json object
+tweets = lines.map(lambda v: json.loads(v))
 
-    #Extract the text field from the json object
-    text_dstream = tweets.map(lambda tweet: tweet['text'])
-    id_dstream = tweets.map(lambda tweet: tweet['id'])
-    text_dstream.pprint()
-
-
-    #Goup by the id
-    id_count_groupBy = id_dstream.countByValue()
-    id_count_groupBy.pprint()
-
-    #Count tweet by text
-    author_counts = text_dstream.countByValue()
-    author_counts.pprint()
-
-    #Convert the dstream to dataframes object ==> so here we have a local context all stream receveid will be in the dataframes
-    df = text_dstream.toDF(schema=schema)
-
-    #Apply the features hashing to the local df (we should apply the same features hashing as the train data )
-    tokenizer = Tokenizer(inputCol="SentimentText", outputCol="SetimentTextTokenize")
-    wordsData = tokenizer.transform(df)
-
-    hashingTF = HashingTF(inputCol="SetimentTextTokenize", outputCol="rawFeatures", numFeatures=1000)
-    featurizedData = hashingTF.transform(wordsData)
-
-    #Show the raw features of the local context df
-    featurizedData.select("rawFeatures").show()
-
-    #Convert the test datafrale to a RDD
-    test_rdd = featurizedData.select("rawFeatures").rdd
-
-    #Convert the spark RDD to LabeledPoint object, this object will be the input of our NaiveBayes classifiers models
-    test = test_rdd.map(lambda p: (model.predict(p.rawFeatures)))
-    test.pprint()
-except:
-    print "Error occuring"
-
-#counts = tweets.flatMap(lambda line: line.split(" ").map(lambda word: (word,1)).reduceByKey(lambda a, b: a+b))
-#counts.pprint()
+#Extract the text field from the json object
+text_dstream = tweets.map(lambda tweet: tweet['text'])
+id_dstream = tweets.map(lambda tweet: tweet['id'])
+#text_dstream.pprint()
 
 
-#print lines
+#Goup by the id
+id_count_groupBy = id_dstream.countByValue()
+id_count_groupBy.pprint()
+
+#Count tweet by text
+author_counts = text_dstream.countByValue()
+author_counts.pprint()
+
+
+df = text_dstream.foreachRDD(lambda rdd: predict(rdd))
+
+
+
 ssc.start()             # Start the computation
 ssc.awaitTermination()  # Wait for the computation to terminate
+
+
+#Convert the dstream to dataframes object ==> so here we have a local context all stream receveid will be in the dataframes
+#df = text_dstream.transform(lambda rdd: sc.parallelize(rdd))
+#hashingTF = HashingTF()
+#predictions = text_dstream.foreachRDD(lambda rdd: predict(rdd))
+
+
+#Apply the features hashing to the local df (we should apply the same features hashing as the train data )
+#tokenizer = Tokenizer(inputCol="SentimentText", outputCol="SetimentTextTokenize")
+#wordsData = tokenizer.transform(df)
+
+#hashingTF = HashingTF(inputCol="SetimentTextTokenize", outputCol="rawFeatures", numFeatures=1000)
+#featurizedData = hashingTF.transform(wordsData)
+
+#Show the raw features of the local context df
+#featurizedData.select("rawFeatures").show()
+
+#Convert the test datafrale to a RDD
+#test_rdd = featurizedData.select("rawFeatures").rdd
+
+#Convert the spark RDD to LabeledPoint object, this object will be the input of our NaiveBayes classifiers models
+#test = test_rdd.map(lambda p: (model.predict(p.rawFeatures)))
+#test.pprint
